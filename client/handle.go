79b174ecb56fcc2fccf02999b27aeb29edc69015
wendy512 <wendy512@yeah.net>
2024-07-16 14:28:05 +0800

    fix reconnect wait bug

--- client/handle.go
+++ client/handle.go
@@ -32,42 +32,42 @@ type clientHandler struct {
 }
 
 // InterrogationHandler 总召唤回复
-func (h *clientHandler) InterrogationHandler(conn asdu.Connect, rxAsdu *asdu.ASDU) error {
+func (h *clientHandler) InterrogationHandler(_ asdu.Connect, rxAsdu *asdu.ASDU) error {
 	return h.call.OnInterrogation(rxAsdu)
 }
 
 // CounterInterrogationHandler 总计数器回复
-func (h *clientHandler) CounterInterrogationHandler(conn asdu.Connect, rxAsdu *asdu.ASDU) error {
+func (h *clientHandler) CounterInterrogationHandler(_ asdu.Connect, rxAsdu *asdu.ASDU) error {
 	return h.call.OnCounterInterrogation(rxAsdu)
 }
 
 // ReadHandler 读定值回复
-func (h *clientHandler) ReadHandler(conn asdu.Connect, rxAsdu *asdu.ASDU) error {
+func (h *clientHandler) ReadHandler(_ asdu.Connect, rxAsdu *asdu.ASDU) error {
 	return h.call.OnRead(rxAsdu)
 }
 
 // TestCommandHandler 测试下发回复
-func (h *clientHandler) TestCommandHandler(conn asdu.Connect, rxAsdu *asdu.ASDU) error {
+func (h *clientHandler) TestCommandHandler(_ asdu.Connect, rxAsdu *asdu.ASDU) error {
 	return h.call.OnTestCommand(rxAsdu)
 }
 
 // ClockSyncHandler 时钟同步回复
-func (h *clientHandler) ClockSyncHandler(conn asdu.Connect, rxAsdu *asdu.ASDU) error {
+func (h *clientHandler) ClockSyncHandler(_ asdu.Connect, rxAsdu *asdu.ASDU) error {
 	return h.call.OnClockSync(rxAsdu)
 }
 
 // ResetProcessHandler 进程重置回复
-func (h *clientHandler) ResetProcessHandler(conn asdu.Connect, rxAsdu *asdu.ASDU) error {
+func (h *clientHandler) ResetProcessHandler(_ asdu.Connect, rxAsdu *asdu.ASDU) error {
 	return h.call.OnResetProcess(rxAsdu)
 }
 
 // DelayAcquisitionHandler 延迟获取回复
-func (h *clientHandler) DelayAcquisitionHandler(conn asdu.Connect, rxAsdu *asdu.ASDU) error {
+func (h *clientHandler) DelayAcquisitionHandler(_ asdu.Connect, rxAsdu *asdu.ASDU) error {
 	return h.call.OnDelayAcquisition(rxAsdu)
 }
 
 // ASDUHandler ASDU上报，ASDU数据
-func (h *clientHandler) ASDUHandler(conn asdu.Connect, rxAsdu *asdu.ASDU) error {
+func (h *clientHandler) ASDUHandler(_ asdu.Connect, rxAsdu *asdu.ASDU) error {
 	return h.call.OnASDU(rxAsdu)
 }
 
@@ -111,7 +111,7 @@ func GetDataType(typeId asdu.TypeID) DataType {
 		return SetPointCommandFloatInfo
 	case asdu.C_BO_NA_1, asdu.C_BO_TA_1:
 		return BitsString32CommandInfo
+	default:
+		return UNKNOWN
 	}
-
-	return UNKNOWN
 }
